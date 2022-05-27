d3c9432548149f492fe4d77d4b866b874a3230d8
avalmercafe02@gmail.com <allenkim@Allens-MacBook-Pro.local>
2022-05-27 03:33:15 -0400

    commit tree d02f10640901d1cecfb97a7f0058d7885e31e411

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const invitees = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(invitees, event) {
+    const inviteesMessage = []
+    for(let i=0; i<invitees.length; i++) {
+        inviteesMessage.push(`Thank you, ${invitees[i]}, for the wonderful surprise gift!`)
+    }
+    return inviteesMessage
+}
+
+function countDown () {
+    let i = 11
+    while (i>0){
+        i--
+        console.log(i);
+        
+    }
+    
+}
